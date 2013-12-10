f1c3a8ea16e756be2d1838889effb376c822702c
Don Yu <don@betterment.com>
2013-12-10 15:06:44 -0500

    fixed bootstrap issue with modals

---
+++ app.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+   res.sendfile(__dirname + "/index.html");
+});
+    
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
